29d3ff2e5bfd523a44b2183babf2c5f2bfc5187f
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-01-01 23:02:21 +0900

    ForEach, custom colors

--- DDChartsDemo3/ContentView.swift
+++ DDChartsDemo3/ContentView.swift
@@ -6,19 +6,42 @@
 //
 
 import SwiftUI
+import Charts
 
 struct ContentView: View {
+    let dailySales: [DailySalesType]
+    let min: Double
+    let max: Double
+    let barColors: [Color]
+    let xAxisMarkPosition: AxisMarkPosition = .bottom
+    let yAxisMarkPosition: AxisMarkPosition = .leading
+    @State private var isVerticalChart = true
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Chart Demo 3")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+            
+            Chart {
+                ForEach(dailySales) { item in
+                    BarMark(
+                        x: .value("Day", item.day),
+                        y: .value("Sales", item.sales)
+                    )
+                    .foregroundStyle(by: .value("Day", item.day))
+                }
+            }
         }
         .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    ContentView(
+        dailySales: defaultDailySales,
+        min: 0.0,
+        max: 700.0,
+        barColors: defaultBarColors
+    )
 }
